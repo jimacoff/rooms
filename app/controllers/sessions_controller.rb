1df018c7efc0eaca072c0b349d15e99ce8770c5c
Owen <o.oconnor7@nuigalway.ie>
2014-11-26 20:07:41 +0000

    Edits

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
   skip_before_action :authorize
+
+
   def new
   end
 
@@ -7,7 +9,8 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:name])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to bookings_url
+      redirect_to admin_url
+      ##redirect_to request.referer
     else
       redirect_to login_url, alert: "Invalid user/password combination"
     end
